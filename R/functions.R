213f185a98d7ba0ccfeb3828102771fd04ed7995
Anna Cetnarowska <anna.cetnarowska@gmail.com>
2024-12-10 16:24:13 +0100

    added function updated learning code

---
+++ R/functions.R
@@ -0,0 +1,17 @@
+descriptive_stats <- function(data) {
+    data |>
+        dplyr::group_by(metabolite) |>
+        dplyr::summarise(
+            dplyr::across(
+                value,
+                list(
+                    mean = mean,
+                    sd = sd
+                )
+            )
+        ) |>
+        dplyr::mutate(dplyr::across(
+            tidyselect::where(is.numeric),
+            ~round(.x, digits = 1)
+        ))
+}
